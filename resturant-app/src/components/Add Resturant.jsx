7aa41ffbc88f50539af59740655137a1e65ccf36
sunus <sunusukesan96@gmail.com>
2026-01-03 20:35:10 +0530

    first message

--- resturant-app/src/components/Add Resturant.jsx
+++ resturant-app/src/components/Add Resturant.jsx
@@ -1,2 +1,50 @@
 import {useState}from "react"
-const types=[""]
+const types=["Rajasthani",
+    "Gujarati",
+    "Jain",
+    "Tamil Nadu",
+    "Kerala"
+];
+export default function AddRetaurant({onAdd}){
+    const[ form ,setForm]=useState({
+        restaurantName:"",
+        address:"",
+        type:"",
+        parkingLot:"",
+        Image:"https://via.placeholder.com/150"
+
+    })
+    const handleAdd=()=>{
+        if(!form.restaurantName||!form.address||!form.type||!form.parkingLot===""){
+            alert("All fieldsare required");
+            return;
+        }
+    }
+    onAdd({ form,restaurantID:Date.now(),
+        parkingLot:formparkingLot==="true"
+
+    })
+    alert("Restaurant added");
+    setForm({
+        restaurantName:"",
+        address:"",
+        type:"",
+        parkingLot:"",
+        image:"https://via.placeholder.com/150"
+
+    }
+
+    )
+}
+return(
+    <div className="sidebar">
+        <h3>Add Restaurant</h3>
+        <input placeholder="Name" value="{form.restaurantName}" onChange={e=>setForm({...form,restaurantName:e.target.value})}>
+        </input>
+        <input placeholder="Address" value="{form.Address}" onChange={e=>setForm({...form,Address:e.target.value})}>
+        </input>
+        <select onchange={e=>setForm({...form,parkingLot:e.target.value})}></select>
+        <option value=""Parking></option>
+         <option value=""Parking></option>
+    </div>
+)
